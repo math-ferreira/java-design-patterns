e8ba17b298e89432fb8dc6442ff9dcf1bffb18fb
math-ferreira <mat.s.ferreira@gmail.com>
2022-01-21 23:24:06 -0300

    fly weight pattern

---
+++ src/com/bharath/patterns/flyweight/Shape.java
@@ -0,0 +1,10 @@
+package com.bharath.patterns.flyweight;
+
+public abstract class Shape {
+
+    void draw(int radius, String fillColor, String lineColor) {
+    }
+
+    void draw(int length, int breadth, String fillStyle) {
+    }
+}
---
+++ src/com/bharath/patterns/flyweight/Test.java
@@ -0,0 +1,10 @@
+package com.bharath.patterns.flyweight;
+
+public class Test {
+
+    public static void main(String[] args) {
+        PaintApp paintApp = new PaintApp();
+        paintApp.render(10);
+    }
+
+}
